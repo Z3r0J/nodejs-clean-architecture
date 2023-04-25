b1df024e24bc37e93ab8631c9f4cd97bd31aec88
Z3r0J <tibboxcentral@gmail.com>
2023-04-25 13:16:08 -0400

    ini: initial commit

---
+++ src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entity/User"
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "test",
+    password: "test",
+    database: "test",
+    synchronize: true,
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+})
